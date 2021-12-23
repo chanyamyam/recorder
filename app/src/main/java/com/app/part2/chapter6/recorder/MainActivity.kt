2e2a159b464ca4565ce2cdef33a286dd91184f87
chanyamyam <cksdudtj0221@gmail.com>
2021-12-24 03:18:33 +0900

    voice visualize

--- app/src/main/java/com/app/part2/chapter6/recorder/MainActivity.kt
+++ app/src/main/java/com/app/part2/chapter6/recorder/MainActivity.kt
@@ -1,11 +1,150 @@
 package com.app.part2.chapter6.recorder
 
+import android.content.pm.PackageManager
+import android.icu.text.AlphabeticIndex
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity.apply
+import android.widget.Button
+import androidx.core.view.GravityCompat.apply
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+    private val soundVisualizerView: SoundVisualizerView by lazy {
+        findViewById(R.id.soundVisualizer)
+    }
+    private val recordTimeTextView: CountUpView by lazy {
+        findViewById(R.id.recordTimeTextView)
+    }
+    private val resetButton: Button by lazy {
+        findViewById<Button>(R.id.resetButton)
+    }
+    private val recordButton: RecordButton by lazy {
+        findViewById(R.id.recordButton)
+    }
+
+    private val recordingFilePath: String by lazy{
+        externalCacheDir?.absolutePath + "/recording.3gp"
+    }
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
+    private val requiredPermissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
+    private var state = State.BEFORE_RECORDING
+    set(value) {
+        field = value
+        resetButton.isEnabled = (value == State.AFTER_RECORDING) || (value == State.ON_PLAYING)
+        recordButton.updateIconWithState(value)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestAudioPermission()
+        initViews()
+        bindViews()
+        initVariables()
+    }
+
+    private fun initViews() {
+        recordButton.updateIconWithState(state)
+    }
+    private fun bindViews() {
+        soundVisualizerView.onRequestCurrentAmplitude = {
+            recorder?.maxAmplitude ?: 0
+        }
+        resetButton.setOnClickListener {
+            stopPlaying()
+            state = State.BEFORE_RECORDING
+        }
+        recordButton.setOnClickListener {
+            when(state) {
+                State.BEFORE_RECORDING -> {
+                    startRecording()
+                }
+                State.ON_RECORDING -> {
+                    stopRecording()
+                }
+                State.AFTER_RECORDING -> {
+                    startPlaying()
+                }
+                State.ON_PLAYING -> {
+                    stopPlaying()
+                }
+            }
+        }
+    }
+    private fun initVariables() {
+        state = State.BEFORE_RECORDING
+    }
+
+    private fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setOutputFile(recordingFilePath)// internal은 용량이 부족할 수 있기때문에 external 사용
+            prepare()
+        }
+        recorder?.start()
+        soundVisualizerView.startVisualizing(false)
+        recordTimeTextView.startCountUp()
+        state = State.ON_RECORDING
+    }
+    private fun stopRecording() {
+        recorder?.run {
+            stop()
+            release()
+        }
+        recorder = null
+        soundVisualizerView.stopVisualiszing()
+        recordTimeTextView.stopCountUp()
+        state = State.AFTER_RECORDING
+    }
+    private fun startPlaying() {
+        player = MediaPlayer()
+            .apply {
+                setDataSource(recordingFilePath)
+                prepare()
+            }
+        player?.start()
+        soundVisualizerView.startVisualizing(true)
+        recordTimeTextView.startCountUp()
+        state = State.ON_PLAYING
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+        soundVisualizerView.stopVisualiszing()
+        recordTimeTextView.stopCountUp()
+        state = State.AFTER_RECORDING
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted = requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if(!audioRecordPermissionGranted) {
+            finish()
+        }
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    private fun requestPermissions() {
+    }
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
     }
 }
---
+++ app/src/main/java/com/app/part2/chapter6/recorder/State.kt
@@ -0,0 +1,8 @@
+package com.app.part2.chapter6.recorder
+
+enum class State {
+    BEFORE_RECORDING,
+    ON_RECORDING,
+    AFTER_RECORDING,
+    ON_PLAYING
+}
